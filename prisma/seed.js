0072c5a52829c2df941bc081e004e4e322addbb9
DMPMS <monteiro_davi@yahoo.com>
2025-04-06 13:10:36 -0300

    Migrations para criar e inserir categorias, e ajustando atributos de usuário

---
+++ prisma/seed.js
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.categoria.createMany({
+    data: [
+      { nome: "Outro", prioridade: 1 },
+      { nome: "Documento", prioridade: 2 },
+      { nome: "Relógio", prioridade: 1 },
+      { nome: "Roupa", prioridade: 1 },
+      { nome: "Eletrônico", prioridade: 1 },
+      { nome: "Celular", prioridade: 3 },
+      { nome: "Câmera", prioridade: 2 },
+      { nome: "Notebook", prioridade: 3 },
+      { nome: "Tablet", prioridade: 3 },
+    ],
+    skipDuplicates: true,
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(() => prisma.$disconnect());
