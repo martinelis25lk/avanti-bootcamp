64dd9cdc5c8b2522778d3106cae1bb88f9b89664
WilghanerSilva <wilghener@gmail.com>
2025-04-12 10:15:27 -0300

    Correção de um bug no middleware de upload de imagens

--- src/middlewares/ImgUploadMiddleware.js
+++ src/middlewares/ImgUploadMiddleware.js
@@ -1,8 +1,14 @@
 import multer from "multer";
 import path from "path";
+import * as fs from "fs";
 
 const storage = multer.diskStorage({
   destination: (request, file, cb) => {
+    const caminhoDiretorio = path.join(process.cwd(), "imagens");
+
+    if(!fs.existsSync(caminhoDiretorio))
+      fs.mkdirSync(caminhoDiretorio, {recursive:true});
+
     cb(null, "imagens/");
   },
   filename: (request, file, cb) => {
