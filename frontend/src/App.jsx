6a3d5fae52d2224bac0841661f0d10abee3cbd9d
martinelis25lk <gui.almeida.edu@gmail.com>
2025-04-24 00:19:52 -0300

    feat: Adiciona a tela de Meus Itens

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,17 @@
 import './App.css'
-import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom"
-import { Login } from "./pages/login"
+import { MeusItens } from "./pages/MeusItems";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Login } from "./pages/login";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<MeusItens />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
